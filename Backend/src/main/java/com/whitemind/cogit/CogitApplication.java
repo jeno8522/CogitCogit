510ed6f694d351f4bc20a3bffa35b3fa61b202b9
tjdwnsmm <tjdwnsmm@naver.com>
2023-10-23 16:13:18 +0900

    #S09P31A109-235 init: 패키지 구조 생성

---
+++ Backend/src/main/java/com/whitemind/cogit/CogitApplication.java
@@ -0,0 +1,13 @@
+package com.whitemind.cogit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CogitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CogitApplication.class, args);
+    }
+
+}
