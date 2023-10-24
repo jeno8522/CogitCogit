fa97168afab1b7ae29edd5002469e21fccb5a96f
hyunchul1998 <hyunchul1998@naver.com>
2023-10-24 10:55:41 +0900

    feat: github 유저 정보 요청

--- Backend/src/main/java/com/whitemind/cogit/auth/service/GithubService.java
+++ Backend/src/main/java/com/whitemind/cogit/auth/service/GithubService.java
@@ -1,5 +1,7 @@
 package com.whitemind.cogit.auth.service;
 
+import lombok.extern.slf4j.Slf4j;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +10,7 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+@Slf4j
 @Service
 public class GithubService {
     @Value("${GITHUB_CLIENT_ID}")
@@ -15,7 +18,25 @@ public class GithubService {
 
     @Value("${GITHUB_CLIENT_SECRET}")
     private String clientSecret;
-    public String getAccessToken(String code) throws IOException {
+
+    public void getGithubUserInfo(String accessToken) throws Exception {
+        log.info("getGithubUserInfo | 유저 정보 요청");
+        URL url = new URL("https://api.github.com/user");
+        HttpURLConnection conn = (HttpURLConnection)  url.openConnection();
+        conn.setDoInput(true);
+        conn.setDoOutput(true);
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Accept", "application/json");
+        conn.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36");
+        conn.setRequestProperty("Authorization","Bearer " + accessToken);
+        int responseCode = conn.getResponseCode();
+        System.out.println(responseCode);
+        String responseData = getResponse(conn, responseCode);
+        System.out.println(responseData);
+        conn.disconnect();
+    }
+
+    public String getAccessToken(String code) throws Exception {
         URL url = new URL("https://github.com/login/oauth/access_token");
         HttpURLConnection conn = (HttpURLConnection)  url.openConnection();
         conn.setDoInput(true);
@@ -34,7 +55,10 @@ public class GithubService {
         int responseCode = conn.getResponseCode();
 
         String responseData = getResponse(conn, responseCode);
+        JSONObject jObject = new JSONObject(responseData);
 
+        String accessToken = jObject.getString("access_token");
+        getGithubUserInfo(accessToken);
         conn.disconnect();
 
         return responseData;
