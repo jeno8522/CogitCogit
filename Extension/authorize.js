aa3462c65672c489afab2812077e867d4a01dfb3
hyunchul1998 <hyunchul1998@naver.com>
2023-10-27 11:02:53 +0900

    feat: Cogit 로그인 요청 및 토큰 저장

--- Extension/authorize.js
+++ Extension/authorize.js
@@ -1,24 +1,55 @@
+// url 인가코드 파싱
 function parseAccessCode(url) {
-    if (url.match(/\?error=(.+)/)) {
-        // TODO: 오류 알림을 띄우고 창을 닫아야함
-    } else {
-      // eslint-disable-next-line
-      const accessCode = url.match(/\?code=([\w\/\-]+)/);
-      if (accessCode) {
-        console.log(accessCode[1]);
-        // TODO: 인가코드를 사용해서 백엔드 api/member/github에 get 요청 보내야함
-      }
+  if (url.match(/\?error=(.+)/)) {
+    // TODO: 오류 알림을 띄우고 창을 닫아야함
+    return null;
+  } else {
+    // eslint-disable-next-line
+    const accessCode = url.match(/\?code=([\w\/\-]+)/);
+    if (accessCode) {
+      return accessCode[1];
     }
+  }
 }
 
+// 코깃코깃 서비스 로그인
+function requestCogitLogin(code) {
+  console.log("parameter", code);
+
+  fetch(`http://localhost:8080/member/github?code=${code}`, {
+    method: "GET",
+  }).then((response) => {
+    // 응답헤어 로큰 익스텐션 로컬스토리지 저장
+    console.log(response.headers.get("Authorization"));
+    chrome.storage.local.set(
+      {
+        cogit: {
+          Authorization: response.headers.get("Authorization"),
+          RefreshToken: response.headers.get("RefreshToken"),
+        },
+      },
+      () => {
+        // 저장 되었는지 확인
+        chrome.storage.local.get("cogit", (data) => {
+          console.log(data);
+        });
+        window.close();
+      }
+    );
+  });
+}
 
 /* Check for open pipe */
-if (window.location.host === 'github.com') {
-    const link = window.location.href;
-    chrome.storage.local.get('pipe_cogit', (data) => {
+if (window.location.host === "github.com") {
+  const link = window.location.href;
+  chrome.storage.local.get("pipe_cogit", (data) => {
     if (data && data.pipe_cogit) {
-        parseAccessCode(link);
-        chrome.storage.local.remove("pipe_cogit");
+      const code = parseAccessCode(link);
+      if (code != null) {
+        requestCogitLogin(code);
+      }
+
+      chrome.storage.local.remove("pipe_cogit");
     }
-    });
+  });
 }
--- Extension/contentscript.js
+++ Extension/contentscript.js
@@ -1,6 +1,6 @@
-const submitButton = document.getElementById('submit_button');
+const submitButton = document.getElementById("submit_button");
 if (submitButton != null) {
-  submitButton.addEventListener('click', function () {
+  submitButton.addEventListener("click", function () {
     saveCode();
   });
 }
@@ -14,49 +14,52 @@ if (submitButton != null) {
 
 function saveCode() {
   // CodeMirror-line 클래스를 가지고 있는 모든 요소를 선택합니다.
-  const codeMirrorLines = document.querySelectorAll('.CodeMirror-line');
+  const codeMirrorLines = document.querySelectorAll(".CodeMirror-line");
 
   // 각 요소에 포함된 내용을 추출하여 배열에 저장합니다.
-  const linesContent = Array.from(codeMirrorLines).map((line) => line.textContent);
+  const linesContent = Array.from(codeMirrorLines).map(
+    (line) => line.textContent
+  );
 
   // 각 내용을 줄바꿈으로 연결합니다.
-  const contentWithNewlines = linesContent.join('\n');
+  const contentWithNewlines = linesContent.join("\n");
 
   // 결과를 출력하거나 다른 작업을 수행합니다.
   console.log(contentWithNewlines);
 
-  localStorage.setItem('code', contentWithNewlines);
+  localStorage.setItem("code", contentWithNewlines);
 }
 
 let solutionElements = document.querySelectorAll('[id^="solution-"]');
 
-let preContent = '';
+let preContent = "";
 if (solutionElements.length > 0) {
   let firstSolutionElement = solutionElements[0];
-  let spanElement = firstSolutionElement.querySelector('span[data-color]');
+  let spanElement = firstSolutionElement.querySelector("span[data-color]");
   preContent = spanElement.textContent;
   console.log(preContent);
   if (
-    preContent.includes('채점 중') ||
-    preContent.includes('기다리는 중') ||
-    preContent.includes('채점 준비 중') ||
-    preContent.includes('중')
+    preContent.includes("채점 중") ||
+    preContent.includes("기다리는 중") ||
+    preContent.includes("채점 준비 중") ||
+    preContent.includes("중")
   ) {
     // 2초 간격으로 내용 확인
     const intervalId = setInterval(function () {
       solutionElements = document.querySelectorAll('[id^="solution-"]');
       firstSolutionElement = solutionElements[0];
-      spanElement = firstSolutionElement.querySelector('span[data-color]');
+      spanElement = firstSolutionElement.querySelector("span[data-color]");
 
       if (spanElement) {
         let currentContent = spanElement.textContent;
 
-        if (currentContent === preContent && !currentContent.includes('중')) {
+        if (currentContent === preContent && !currentContent.includes("중")) {
           clearInterval(intervalId); // setInterval 중지
           let codeLanguage = firstSolutionElement
-            .querySelector('td:nth-child(7)')
-            .querySelector('a').textContent;
-          let codeRunningTime = firstSolutionElement.querySelector('.time').textContent;
+            .querySelector("td:nth-child(7)")
+            .querySelector("a").textContent;
+          let codeRunningTime =
+            firstSolutionElement.querySelector(".time").textContent;
 
           sendCode(currentContent, codeLanguage, codeRunningTime);
         }
@@ -65,19 +68,19 @@ if (solutionElements.length > 0) {
       }
     }, 2000);
   } else {
-    console.log('이미 제출한 코드입니다.');
+    console.log("이미 제출한 코드입니다.");
   }
 }
 
 function sendCode(textValue, codeLanguage, codeRunningTime) {
   console.log(textValue);
-  if (localStorage.getItem('code')) {
-    var code = localStorage.getItem('code');
+  if (localStorage.getItem("code")) {
+    var code = localStorage.getItem("code");
 
-    fetch('http://localhost:8080/code', {
-      method: 'POST',
+    fetch("http://localhost:8080/code", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         codeContent: code,
