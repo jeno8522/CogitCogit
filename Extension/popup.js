6b66c91671d96fc82b67e6b88a87d1807d189d78
haeun <kodms08@naver.com>
2023-10-31 16:33:03 +0900

    feat: 요청 헤더 jwt 토큰 추가

--- Extension/popup.js
+++ Extension/popup.js
@@ -1,22 +1,22 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener('DOMContentLoaded', function () {
   // 페이지가 로드될 때 실행할 코드
-  const AUTHORIZATION_URL = "https://github.com/login/oauth/authorize";
-  const CLIENT_ID = "8f0485d786b3f5eba00e";
-  const REDIRECT_URL = "https://github.com/"; // 변경 필요
+  const AUTHORIZATION_URL = 'https://github.com/login/oauth/authorize';
+  const CLIENT_ID = '8f0485d786b3f5eba00e';
+  const REDIRECT_URL = 'https://github.com/'; // 변경 필요
   const SCOPES = [
-    "repo",
-    "admin:repo_hook",
-    "admin:org",
-    "admin:public_key",
-    "admin:org_hook",
-    "user",
-    "project",
+    'repo',
+    'admin:repo_hook',
+    'admin:org',
+    'admin:public_key',
+    'admin:org_hook',
+    'user',
+    'project',
   ];
 
   function login() {
     let url = `${AUTHORIZATION_URL}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL}&scope=`;
     for (let i = 0; i < SCOPES.length; i++) {
-      url += SCOPES[i];
+      url += SCOPES[i] + ' ';
     }
 
     chrome.storage.local.set({ pipe_cogit: true }, () => {
@@ -28,15 +28,15 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
-  const loginButton = document.getElementById("authenticate");
-  loginButton.addEventListener("click", login);
+  const loginButton = document.getElementById('authenticate');
+  loginButton.addEventListener('click', login);
 
-  chrome.storage.local.get("cogit", function (data) {
+  chrome.storage.local.get('cogit', function (data) {
     if (data.cogit) {
       // cogit 인증 데이터가 존재하면, 로그인 버튼 숨김
-      var authModeElement = document.getElementById("auth_mode");
+      var authModeElement = document.getElementById('auth_mode');
       if (authModeElement) {
-        authModeElement.style.display = "none";
+        authModeElement.style.display = 'none';
       }
     }
   });
--- Extension/sendCode.js
+++ Extension/sendCode.js
@@ -6,28 +6,47 @@ function sendCode(
   codeRunningTime,
   algorithmQuestId
 ) {
-  fetch('http://localhost:8080/code', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      algorithmQuestPlatform: algorithmQuestPlatform,
-      algorithmQuestId: algorithmQuestId,
-      codeContent: codeContent,
-      codeResult: codeResult,
-      codeRunningTime: codeRunningTime,
-      codeLanguage: codeLanguage,
-    }),
-  })
-    .then((response) => {
-      if (response.ok) {
-        return response.text();
-      } else {
-        throw new Error('코드르 전송하지 못했습니다.');
-      }
+  let jwt;
+  chrome.storage.local.get('cogit', function (result) {
+    const cogitData = result.cogit;
+
+    if (cogitData === undefined) {
+      console.log('코깃 데이터 없음');
+      return;
+    }
+
+    // console.log(cogitData);
+    jwt = cogitData.RefreshToken;
+    console.log(jwt);
+    if (jwt === undefined) {
+      console.log('토큰 없음');
+      return;
+    }
+
+    fetch('http://localhost:8080/code', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: jwt,
+      },
+      body: JSON.stringify({
+        algorithmQuestPlatform: algorithmQuestPlatform,
+        algorithmQuestId: algorithmQuestId,
+        codeContent: codeContent,
+        codeResult: codeResult,
+        codeRunningTime: codeRunningTime,
+        codeLanguage: codeLanguage,
+      }),
     })
-    .then((data) => {
-      console.log(data);
-    });
+      .then((response) => {
+        if (response.ok) {
+          return response.text();
+        } else {
+          throw new Error('코드르 전송하지 못했습니다.');
+        }
+      })
+      .then((data) => {
+        console.log(data);
+      });
+  });
 }
