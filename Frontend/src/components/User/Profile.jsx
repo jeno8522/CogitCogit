6c0650c895093606a513636e67b1ac658c4e488d
연제정 <94yjj@daum.net>
2023-11-01 15:58:16 +0900

    chore: Profile.jsx className 오타 수정

--- Frontend/src/components/User/Profile.jsx
+++ Frontend/src/components/User/Profile.jsx
@@ -6,7 +6,7 @@ function Profile() {
   return (
     <div>
       <button className="w-10 h-10 text-xl font-bold text-white rounded-full bg-hover">
-        <span classNAme="relative mb-1">닉</span>
+        <span className="relative mb-1">닉</span>
       </button>
     </div>
   );
