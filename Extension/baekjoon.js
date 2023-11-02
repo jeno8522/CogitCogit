b57e7c7985c6b324f822498c39ce5f72f0a88ea4
haeun <kodms08@naver.com>
2023-11-02 16:59:47 +0900

    feat: 백준 코드 전송 dto 변경

--- Extension/baekjoon.js
+++ Extension/baekjoon.js
@@ -42,7 +42,10 @@ if (solutionElements.length > 0) {
           clearInterval(intervalId); // setInterval 중지
           let codeLanguage = firstSolutionElement
             .querySelector('td:nth-child(7)')
-            .querySelector('a').textContent;
+            .querySelector('a')
+            .textContent.trim();
+          codeLanguage = baekjoonLanguages[codeLanguage];
+          let codeFileExtension = baekjoonExtension[codeLanguage];
           let codeRunningTime = firstSolutionElement.querySelector('.time').textContent;
           let algorithmQuestId = firstSolutionElement
             .querySelector('td:nth-child(3)')
@@ -51,28 +54,34 @@ if (solutionElements.length > 0) {
           if (localStorage.getItem('code')) {
             var code = localStorage.getItem('code');
 
-            if (
-              currentContent.includes('맞았습니다') ||
+            if (currentContent.includes('맞았습니다')) {
+              sendCode(
+                code,
+                true,
+                'BAEKJOON',
+                codeLanguage,
+                codeRunningTime,
+                algorithmQuestId,
+                codeFileExtension
+              );
+              var cogitImg = document.createElement('img');
+              cogitImg.src = 'https://cogitusercode.s3.ap-northeast-2.amazonaws.com/cogit.PNG';
+              cogitImg.style = 'width:20px';
+              spanElement.appendChild(cogitImg);
+            } else if (
               currentContent.includes('틀렸습니다') ||
               currentContent.includes('시간 초과') ||
               currentContent.includes('메모리 초과')
             ) {
               sendCode(
                 code,
-                currentContent,
-                'Baekjoon',
+                false,
+                'BAEKJOON',
                 codeLanguage,
                 codeRunningTime,
-                algorithmQuestId
+                algorithmQuestId,
+                codeFileExtension
               );
-
-              if (currentContent.includes('맞았습니다')) {
-                var cogitImg = document.createElement('img');
-                cogitImg.src =
-                  'https://hexagonal-locket-e6a.notion.site/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2Ffcee80a5-143a-4e25-9abf-0bcf3a18847d%2Fa1d3c27d-0fc6-41af-bc21-21ea7ef1cdd1%2FUntitled.png?table=block&id=3b2ecac2-d9d7-4e8a-8276-ab83a0fb876f&spaceId=fcee80a5-143a-4e25-9abf-0bcf3a18847d&width=2000&userId=&cache=v2';
-                cogitImg.style = 'width:20px';
-                spanElement.appendChild(cogitImg);
-              }
             }
           }
         }
