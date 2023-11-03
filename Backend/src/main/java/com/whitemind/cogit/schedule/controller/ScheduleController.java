62c0985e112a4aa2da351559a2bcb484ddda3151
hyunchul1998 <hyunchul1998@naver.com>
2023-11-03 17:39:48 +0900

    feat: 스케줄 생성 API 구현

--- Backend/src/main/java/com/whitemind/cogit/schedule/controller/ScheduleController.java
+++ Backend/src/main/java/com/whitemind/cogit/schedule/controller/ScheduleController.java
@@ -3,6 +3,7 @@ package com.whitemind.cogit.schedule.controller;
 import com.whitemind.cogit.common.response.ResponseResult;
 import com.whitemind.cogit.member.dto.request.CreateStudyRequest;
 import com.whitemind.cogit.schedule.dto.request.CreateScheduleRequest;
+import com.whitemind.cogit.schedule.service.ScheduleService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,10 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/schedule")
 public class ScheduleController {
+    private final ScheduleService scheduleService;
     @PostMapping("/add")
     public ResponseResult createStudySchetule(@RequestBody CreateScheduleRequest schedule) throws Exception {
         log.info("createStudySchetule | Study 스케줄 생성 요청");
-        System.out.println(schedule.getScheduleName());
+        scheduleService.createSchedule(schedule);
         return ResponseResult.successResponse;
     }
 }
