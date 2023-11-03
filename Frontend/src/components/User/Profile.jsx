f4c8ad4d6b60f5b655f9d64ea496410ebf8d83ea
이현구 <wancern@naver.com>
2023-11-03 15:12:55 +0900

    feat: Profile 드롭다운 기능 추가

--- Frontend/src/components/User/Profile.jsx
+++ Frontend/src/components/User/Profile.jsx
@@ -1,13 +1,23 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
+import ProfileMenu from './ProfileMenu';
 
 function Profile() {
+  const [showMenu, setMenu] = useState(false);
+
+  const onClick = () => {
+    setMenu((prev) => !prev);  
+  };
+
   return (
     <div>
-      <button className="w-10 h-10 text-xl font-bold text-white rounded-full bg-hover">
+      <button className="w-10 h-10 text-xl font-bold text-white rounded-full bg-hover" onClick={onClick}>
         <span className="relative mb-1">닉</span>
       </button>
+      {showMenu && (
+        <ProfileMenu onCloseMenu={() => setMenu(false)} />
+      )}
     </div>
   );
 }
