89103e8c128daec7fbc2114cd8b531c1486674b4
hyunchul1998 <hyunchul1998@naver.com>
2023-11-06 14:24:32 +0900

    feat: 코드 댓글 등록 API 구현

--- Backend/src/main/java/com/whitemind/cogit/common/error/ExceptionCode.java
+++ Backend/src/main/java/com/whitemind/cogit/common/error/ExceptionCode.java
@@ -6,7 +6,8 @@ import lombok.Getter;
 public enum ExceptionCode {
     NOT_EXIST_MEMBER_EXCEPTION(404, "사용자 계정이 존재하지 않습니다."),
     NOT_EXIST_ALGORITHM_PLATFORM_EXCEPTION(404, "잘못된 플랫폼입니다."),
-    NOT_EXIST_TEAM_EXCEPTION(404, "스터디가 존재하지 않습니다.");
+    NOT_EXIST_TEAM_EXCEPTION(404, "스터디가 존재하지 않습니다."),
+    NOT_EXIST_CODE_EXCEPTION(404, "코드가 존재하지 않습니다.");
 
     private final int errorCode;
     private final String errorMessage;
