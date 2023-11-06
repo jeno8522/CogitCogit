f5e5a0a72ef3492df85afef93a3f37b1fb83b5d2
haeun <kodms08@naver.com>
2023-11-06 17:51:13 +0900

    feat: 팀별 페이지 조회 API 구현

--- Backend/src/main/java/com/whitemind/cogit/schedule/repository/ScheduleRepository.java
+++ Backend/src/main/java/com/whitemind/cogit/schedule/repository/ScheduleRepository.java
@@ -1,8 +1,13 @@
 package com.whitemind.cogit.schedule.repository;
 
+import java.util.List;
+
+import com.whitemind.cogit.schedule.dto.GetScheduleDto;
 import com.whitemind.cogit.schedule.entity.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Integer> {
     Schedule findByScheduleId(int scheduleId);
+    List<Schedule> findTop5ByTeamTeamIdOrderByScheduleEndAtDesc(int teamId);
+
 }
