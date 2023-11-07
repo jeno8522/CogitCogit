077e3a92d2a2aff03735f1fe18559c6fe7ff79c2
haeun <kodms08@naver.com>
2023-11-07 15:21:00 +0900

    feat: schedule detail 조회 API 구현

--- Backend/src/main/java/com/whitemind/cogit/schedule/repository/AlgorithmQuestRepository.java
+++ Backend/src/main/java/com/whitemind/cogit/schedule/repository/AlgorithmQuestRepository.java
@@ -1,16 +1,17 @@
 package com.whitemind.cogit.schedule.repository;
 
+import java.util.List;
+
 import com.nimbusds.jose.Algorithm;
 import com.whitemind.cogit.schedule.entity.AlgorithmQuest;
 import com.whitemind.cogit.schedule.entity.Schedule;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface AlgorithmQuestRepository extends JpaRepository<AlgorithmQuest, Integer> {
     AlgorithmQuest findByAlgorithmQuestNumber(int algorithmQuestNumber);
 
-    List<AlgorithmQuest> findAlgorithmQuestsBySchedule(Schedule schedule);
+    List<AlgorithmQuest> findBySchedule(Schedule schedule);
 }
