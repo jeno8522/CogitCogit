9851cd1292831674a6f23cd1012434165fb97f44
이현구 <wancern@naver.com>
2023-11-08 16:58:33 +0900

    feat: 추가 기능 그룹페이지와 연동

--- Frontend/src/app/group/page.jsx
+++ Frontend/src/app/group/page.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 import Ranking from './Ranking/Ranking';
 import Schedule from './Schedule/Schedule';
 import ToDoList from './ToDo/ToDoList';
@@ -8,8 +8,22 @@ import Button from '@/components/Button';
 import SettingIcon from '@/icons/setting.svg';
 import ExitIcon from '@/icons/exit.svg';
 import GroupIcon from '@/icons/group.svg';
+import MemberManagementModal from './Modal/MemberManagementModal';
+import MemberAddModal from './Modal/MemberAddModal';
 
 function page() {
+  const [showMemberManagementModal, setMemberManagementModal] = useState(false);
+  const [showMemberAddModal, setMemberAddModal] = useState(false);
+
+  const onClickMemberManagementModal = () => {
+    setMemberManagementModal((prev) => !prev);
+  };
+
+  const onClickAddMember = () => {
+    setMemberAddModal((prev) => !prev);
+    // setMemberManagementModal((prev) => !prev);
+  };
+
   return (
     <div className="m-7">
       <div className="flex justify-between">
@@ -18,12 +32,25 @@ function page() {
           <div className="pl-2 text-2xl font-bold">하얀마음109</div>
         </div>
         <div>
-          <Button className="p-1 mb-3 mr-3 rounded-small bg-primary">
+          <Button
+            className="p-1 mb-3 mr-3 rounded-small bg-primary"
+            onClick={onClickMemberManagementModal}
+          >
             <SettingIcon alt="settingIcon" width={36} height={36} />
           </Button>
           <Button className="p-1 mb-3 rounded-small bg-hover hover:bg-warning">
             <ExitIcon alt="settingIcon" width={36} height={36} />
           </Button>
+          {showMemberManagementModal && (
+            <MemberManagementModal
+              isOpen={showMemberManagementModal}
+              onClose={() => setMemberManagementModal(false)}
+              onClickAddMember={onClickAddMember}
+            />
+          )}
+          {showMemberAddModal && (
+            <MemberAddModal isOpen={showMemberAddModal} onClose={onClickAddMember} />
+          )}
         </div>
       </div>
       <div className="flex justify-center w-full">
