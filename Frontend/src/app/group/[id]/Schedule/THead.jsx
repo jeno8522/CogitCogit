4d3feb1961d83fa50cfd9eec7d2d590613042650
이현구 <wancern@naver.com>
2023-11-14 10:06:12 +0900

    chore: 테이블 헤더 문제사이트 링크 추가

--- Frontend/src/app/group/[id]/Schedule/THead.jsx
+++ Frontend/src/app/group/[id]/Schedule/THead.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link';
 
 const THead = ({ questions }) => {
   return (
@@ -6,12 +7,14 @@ const THead = ({ questions }) => {
       <thead>
         <tr className="bg-gray-800">
           <th className="px-4 py-3 text-gray-300">이름</th>
-          {questions.map((question) => {
+          {questions.map((question, idx) => {
             return (
-              <th className="px-4 py-3 text-gray-300">
-                {question.algorithmQuestPlatform}
-                <br />
-                {question.algorithmQuestNumber}
+              <th className="px-4 py-3 text-gray-300" key={idx}>
+                <Link href={question.algorithmQuestUrl}>
+                  {question.algorithmQuestPlatform}
+                  <br />
+                  {question.algorithmQuestNumber}
+                </Link>
               </th>
             );
           })}
