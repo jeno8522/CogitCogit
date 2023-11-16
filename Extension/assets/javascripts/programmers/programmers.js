8e5000dc7d69d969dee472d6e8a6be26c98c5dbd
haeun <kodms08@naver.com>
2023-11-16 12:36:24 +0900

    feat: 프로그래머스 제출 icon 추가

--- Extension/assets/javascripts/programmers/programmers.js
+++ Extension/assets/javascripts/programmers/programmers.js
@@ -1,33 +1,25 @@
-const PLATFORM_URL =
-  'https://school.programmers.co.kr/learn/courses/30/lessons/';
+const PLATFORM_URL = 'https://school.programmers.co.kr/learn/courses/30/lessons/';
 
 const submitButton = document.getElementById('submit-code');
 if (submitButton != null) {
   submitButton.addEventListener('click', function () {
-    
-      chrome.storage.local.get('active', function (result) {
-        if (result.active) {
-          if (result.active == 'active') {
-            saveCode();
-          }
+    chrome.storage.local.get('active', function (result) {
+      if (result.active) {
+        if (result.active == 'active') {
+          saveCode();
         }
-      });
-    
+      }
+    });
   });
 }
 
 function saveCode() {
-  let codeLanguage = document
-    .getElementById('tour7')
-    .querySelector('button')
-    .textContent.trim();
+  let codeLanguage = document.getElementById('tour7').querySelector('button').textContent.trim();
   let codeFileExtension = programmersExtension[codeLanguage];
   codeLanguage = programmersLanguages[codeLanguage];
 
   const codeMirrorLines = document.querySelectorAll('.CodeMirror-line');
-  const linesContent = Array.from(codeMirrorLines).map(
-    (line) => line.textContent
-  );
+  const linesContent = Array.from(codeMirrorLines).map((line) => line.textContent);
   const codeContent = linesContent.join('\n');
 
   let codeResult = document.querySelector('.modal-title');
@@ -35,8 +27,15 @@ function saveCode() {
     codeResult.textContent = '로딩중';
   }
 
+  let funcButtons = document.querySelector('.func-buttons');
+  let loadingImg = document.createElement('img');
+  loadingImg.src = 'https://cogitusercode.s3.ap-northeast-2.amazonaws.com/assets/loading.gif';
+  loadingImg.style = 'width:30px';
+  funcButtons.prepend(loadingImg);
+
   const intervalId = setInterval(function () {
-    codeResult = document.querySelector('.modal-title').textContent;
+    let modalTitle = document.querySelector('.modal-title');
+    codeResult = modalTitle.textContent;
     var codeRunningTime = 0;
     var algorithmQuestNumber = document
       .querySelector('div.main > div.lesson-content')
@@ -47,10 +46,11 @@ function saveCode() {
 
       let result = false;
 
+      funcButtons.remove();
+      let cogitImg = document.createElement('img');
+
       if (codeResult.includes('정답')) {
-        const timeElements = document.querySelectorAll(
-          '.console-test-group td.result.passed'
-        );
+        const timeElements = document.querySelectorAll('.console-test-group td.result.passed');
         const runTimes = [];
 
         for (var timeElement of timeElements) {
@@ -59,21 +59,40 @@ function saveCode() {
             runTimes.push(parseFloat(runTime[1]));
           }
         }
-        codeRunningTime =
-          runTimes.reduce((acc, value) => acc + value, 0) / runTimes.length;
+        codeRunningTime = runTimes.reduce((acc, value) => acc + value, 0) / runTimes.length;
         result = true;
-      }
 
-      sendCode(
-        codeContent,
-        result,
-        'PROGRAMMERS',
-        codeLanguage,
-        codeRunningTime,
-        algorithmQuestNumber,
-        codeFileExtension,
-        `${PLATFORM_URL}${algorithmQuestNumber}`
-      );
+        sendCode(
+          codeContent,
+          result,
+          'PROGRAMMERS',
+          codeLanguage,
+          codeRunningTime,
+          algorithmQuestNumber,
+          codeFileExtension,
+          `${PLATFORM_URL}${algorithmQuestNumber}`
+        );
+
+        cogitImg.src = 'https://cogitusercode.s3.ap-northeast-2.amazonaws.com/assets/cogit.png';
+        cogitImg.style = 'width:30px; margin-left:10px';
+        modalTitle.appendChild(cogitImg);
+      } else {
+        sendCode(
+          codeContent,
+          result,
+          'PROGRAMMERS',
+          codeLanguage,
+          codeRunningTime,
+          algorithmQuestNumber,
+          codeFileExtension,
+          `${PLATFORM_URL}${algorithmQuestNumber}`
+        );
+
+        cogitImg.src =
+          'https://cogitusercode.s3.ap-northeast-2.amazonaws.com/assets/cogit_gray.png';
+        cogitImg.style = 'width:30px; margin-left:10px';
+        modalTitle.appendChild(cogitImg);
+      }
     }
   }, 2000);
 }
