cbd8847787ec45cbf7d9900296a864b632f662e3
hyunchul1998 <hyunchul1998@naver.com>
2023-11-16 10:19:47 +0900

    feat: 코드 댓글 조회 API 추가

---
+++ Backend/src/main/java/com/whitemind/cogit/code/dto/response/GetCommentResponse.java
@@ -0,0 +1,19 @@
+package com.whitemind.cogit.code.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Builder
+@Getter
+@Setter
+public class GetCommentResponse {
+    int commentId;
+    int commentLineNumber;
+    int memberId;
+    String memberNickname;
+    String commentContent;
+    LocalDateTime createAt;
+}
